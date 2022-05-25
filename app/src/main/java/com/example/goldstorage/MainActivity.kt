f3ecd9dd802c43beeef088e344cd9386e0c3b445
kevk <kevinku668833@gmail.com>
2022-05-25 02:26:32 -0700

    Added a temporary Progress bar and Menu Buttons

--- app/src/main/java/com/example/goldstorage/MainActivity.kt
+++ app/src/main/java/com/example/goldstorage/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.goldstorage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // This needs to return a boolean to signify if we handled the user's action
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
+            item ->
+
+            when (item.itemId) {
+
+                R.id.action_home -> {
+                    // TODO Navigate to the home screen
+                    Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.action_history -> {
+                    // TODO Navigate to the history screen
+                    Toast.makeText(this, "History", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.action_settings -> {
+                    // TODO Navigate to the edit screen
+                    Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+                }
+            }
+            // Return true to say that we've handled this user interaction on the item
+            true
+        }
     }
 }
---
+++ app/src/main/java/com/example/goldstorage/fragments/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.goldstorage.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import com.example.goldstorage.R
+
+class HomeFragment : Fragment() {
+    private var progr = 0;
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun updateProgressBar() {
+        // TODO implement this
+    }
+}
