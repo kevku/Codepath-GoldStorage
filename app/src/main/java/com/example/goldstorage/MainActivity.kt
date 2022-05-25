8b2f6665275ffd62e54f052039290c408b570b82
beliang <b3nliang@gmail.com>
2022-05-25 02:22:23 -0700

    Activities
    added main and login activities

--- app/src/main/java/com/example/goldstorage/MainActivity.kt
+++ app/src/main/java/com/example/goldstorage/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.goldstorage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.signOut).setOnClickListener {
+            ParseUser.logOutInBackground()
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
